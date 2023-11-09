f12e9611e4ae1a341eb3a2a719b26cb43b6f5da7
William Hatcher <william@hatcher.work>
2023-11-09 14:14:49 -0600

    Django Cookiecutter Init

---
+++ tidal_terrors/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tidal_terrors.users.models import User
+from tidal_terrors.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
